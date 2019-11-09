9e5d3df961377ed9261369ab6cbf4d0dc60b0575
lu lu <luwangdudu@gmail.com>
2019-11-09 01:54:58 -0800

    change js

--- html/lulu.js
+++ html/lulu.js
@@ -15,7 +15,7 @@ var imgInfo = [
             <input type="radio" name="{1}" value="3" />
             <input type="radio" name="{1}" value="4" />
             <input type="radio" name="{1}" value="5" />
-            <span> {2} </span>
+            {2}
           </div>`;
   
   let scaleMetadata = [
@@ -60,14 +60,80 @@ var imgInfo = [
   /*This function will be called when a button is clicked and it will
   update the information displayed in the 'robot' div based on the 
   data stored within robotInfo*/
+
   
   function changeRobot(robotFaceSrc) {
     let robotDiv = document.getElementById("robotFace");
+
+    // console.log(robotDiv);
+
+    // console.log($('#robotFace'));
+
+    // console.log(_(1234));
     robotDiv.src = robotFaceSrc;  
   }
-  
+
+  function test() {
+    const $jqResult = $('.scales span:first-child');
+
+    // $jqResult
+    // .html((index, oldHtml) => {
+    //   return `Baobao ${oldHtml} Lulu ${index}`
+    // })
+    // .css({
+    //   'color': 'gold',
+    //   // 'font-size': '20px', 
+    //   'padding-left': '15px'
+    // });
+
+
+    // $('span').remove();
+
+    const $span = $('<span>123</span>');
+    
+    $span.insertBefore($jqResult);
+  }
 
   
+  function changeScalesOrderBackup1() {
+    
+    let parentContainer = document.getElementsByClassName('section2')[0];  
+    
+    let shuffledMetadata = shuffle(scaleMetadata);
+    
+    for (let i = 0 ; i < shuffledMetadata.length; i ++) {
+      let metadata = shuffledMetadata[i];
+      let color = '';
+      if (i % 2 == 0) {
+        color = 'red';
+      } else {
+        color = 'blue';
+      }
+
+      const $templateElement = $(`
+          <div class="scales">
+            <span></span>
+            <input type="radio" value="1" />
+            <input type="radio" value="2" />
+            <input type="radio" value="3" />
+            <input type="radio" value="4" />
+            <input type="radio" value="5" />
+            <span></span>
+          </div>
+      `);
+
+      const $spans = $templateElement.find('span');
+      $templateElement.find('input').attr('name', metadata.variableName);
+
+      $spans.eq(0).html(metadata.yin);
+      $spans.eq(-1).html(metadata.yang).css('color', color);
+
+      const $container = $(parentContainer);
+
+      $container.append($templateElement);
+    }  
+  }
+
   function changeScalesOrder() {
     
     let parentContainer = document.getElementsByClassName('section2')[0];  
@@ -82,9 +148,28 @@ var imgInfo = [
       } else {
         color = 'blue';
       }
-      let html = scaleTemplate.replace('{0}', metadata.yin)
-                 .replace('{2}', `<span style="color: ${color}">${metadata.yang}</span>`)
-                 .replace(/\{1\}/g, metadata.variableName);
-      parentContainer.innerHTML += html;
+
+      const $input = $('<input/>').attr({
+        'type': 'radio',
+        'name': metadata.variableName,
+        'value': '1'
+      });
+
+      const $templateElement = $('<div/>').addClass('scales')
+      .append($('<span/>').html(metadata.yin))
+      .append($input)
+      .append($input.clone().attr('value', '2'))
+      .append($input.clone().attr('value', '3'))
+      .append($input.clone().attr('value', '4'))
+      .append($input.clone().attr('value', '5'))
+      .append($('<span/>').html(metadata.yang).css('color', color));
+
+      const $container = $(parentContainer);
+
+      $container.append($templateElement);
     }  
   }
+
+
+
+ 
